666a001a27cfb6fa57169d348f584e756e9d0dc1
Miao <hem4@seattleu.edu>
2019-04-17 00:47:52 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -22,146 +22,117 @@
       "Genre": "K-pop"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
+      "Position": "4",
+      "Track Name": "Old Town Road",
       "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
+      "Streams": "3,942,370",
       "Genre": "Hip-Hop/Rap"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
+      "Position": "5",
+      "Track Name": "SOS",
+      "Artist": "Avicii",
+      "Streams": "3,726,784",
+      "Genre": "EDM"
+    },
+    {
+      "Position": "6",
+      "Track Name": "Con Calma",
+      "Artist": "Daddy Yankee",
+      "Streams": "3,464,746",
+      "Genre": "Pop"
+    },
+    {
+      "Position": "7",
+      "Track Name": "7 rings",
+      "Artist": "Ariana Grande",
+      "Streams": "3,270,160",
+      "Genre": "Pop"
+    },
+    {
+      "Position": "8",
+      "Track Name": "Sucker",
+      "Artist": "Jonas Brothers",
+      "Streams": "3,173,041",
+      "Genre": "Pop"
+    },
+    {
+      "Position": "9",
+      "Track Name": "Sunflower - Spider-Man: Into the Spider-Verse",
+      "Artist": "Post Malone",
+      "Streams": "2,978,261",
+      "Genre": "Pop"
+    },
+    {
+      "Position": "10",
+      "Track Name": "bury a friend",
+      "Artist": "Billie Eilish",
+      "Streams": "2,858,482",
+      "Genre": "Alternative/Indie"
+    },
+    {
+      "Position": "11",
+      "Track Name": "Kill This Love",
+      "Artist": "BLACKPINK",
+      "Streams": "2,673,899",
+      "Genre": "K-pop"
+    },
+    {
+      "Position": "12",
+      "Track Name": "Wow.",
+      "Artist": "Post Malone",
+      "Streams": "2,621,989",
       "Genre": "Hip-Hop/Rap"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "13",
+      "Track Name": "Talk",
+      "Artist": "Khalid",
+      "Streams": "2,590,992",
+      "Genre": "R&B/Soul"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "14",
+      "Track Name": "Don't Call Me Up",
+      "Artist": "Mabel",
+      "Streams": "2,520,596",
+      "Genre": "Pop"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "15",
+      "Track Name": "Shallow",
+      "Artist": "Lady Gaga",
+      "Streams": "2,476,259",
+      "Genre": "Country"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "16",
+      "Track Name": "wish you were gay",
+      "Artist": "Billie Eilish",
+      "Streams": "2,435,693",
+      "Genre": "Alternative/Indie"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "17",
+      "Track Name": "when the party's over",
+      "Artist": "Billie Eilish",
+      "Streams": "2,213,316",
+      "Genre": "Alternative/Indie"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
+      "Position": "18",
+      "Track Name": "Dancing With A Stranger (with Normani)",
+      "Artist": "Sam Smith",
+      "Streams": "2,165,044",
+      "Genre": "Pop"
     },
     {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-    {
-      "Position": "2",
-      "Track Name": "Old Town Road Remix",
-      "Artist": "Lil Nas X",
-      "Streams": "4,950,366",
-      "Genre": "Hip-Hop/Rap"
-    },
-       
+      "Position": "19",
+      "Track Name": "Pa Mi Remix",
+      "Artist": "Dalex",
+      "Streams": "2,113,050",
+      "Genre": "Pop"
+    }, 
     {
       "Position": "20",
       "Track Name": "Here With Me",
