64a7e5cbeff0f729652b9eeb427224e7036747bb
Miao <hem4@seattleu.edu>
2019-04-17 00:32:54 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,124 +1,173 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "Position": "1",
+      "Track Name": "bad guy",
+      "Artist": "Billie Eilish",
+      "Streams": "6,348,322",
+      "Genre": "Alternative/Indie"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },  
+        {
+      "Position": "3",
+      "Track Name": "Boy With Luv (feat. Halsey)",
+      "Artist": "BTS",
+      "Streams": "4,270,870",
+      "Genre": "K-pop"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+    {
+      "Position": "2",
+      "Track Name": "Old Town Road Remix",
+      "Artist": "Lil Nas X",
+      "Streams": "4,950,366",
+      "Genre": "Hip-Hop/Rap"
+    },
+       
+    {
+      "Position": "20",
+      "Track Name": "Here With Me",
+      "Artist": "Marshmello",
+      "Streams": "2,075,881",
+      "Genre": "Dance/Electronic"
     }
   ]
 }
